bd3019b299fa411457215d491af47499444091ea
ianxshort <tut42883@temple.edu>
2025-03-11 12:57:09 -0400

    fixed up the spacing and created the actual function fill the page

--- script.js
+++ script.js
@@ -39,27 +39,75 @@ async function extractData(){
 extractData();
 
 async function openPage(){
-    let dataResponse= await fetch("./data.json");
-    let destData= await dataResponse.json();
-    let fillData="";
-    document.getElementById('destination').addEventListener("click",function(action){
-        if(action.target.classList.contains('card'))
+    try{
+        let dataResponse= await fetch("./data.json");
+        let destData= await dataResponse.json();
+        let fillData="";
+        document.getElementById('destination').addEventListener("click",function(action)
+        {
+            if(action.target.classList.contains('card'))
+                window.location.href=`destination.html`;
+            let cardID= parseInt(action.target.id); //set a variable equal to whatever the person clicked 
+            let correctID=destData.find(destination=>destination.id===cardID); //this line will allow us to gain access to the specific object because of the find function
+        
+
+            sessionStorage.setItem("locationData",JSON.stringify(correctID));
+            
             window.location.href=`destination.html`;
-        let cardID= parseInt(action.target.id); //set a variable equal to whatever the person clicked 
-        let correctID=destData.find(destination=>destination.id===cardID); //this line will allow us to gain access to the specific object because of the find function
-      
-
-        sessionStorage.setItem("locationData",JSON.stringify(correctID));
-          
-        window.location.href=`destination.html`;
-
-
-
-    
-
-
-    });
+        });
+    }
+    catch(error)
+    {
+        console.error("error fetching data", error);
+    }
 }
+
+async function fillPage()
+{
+   try
+   {
+        let destinationData=JSON.parse(sessionStorage.getItem("locationData"));
+        let cardData=document.getElementById(destinationData.id);
+        cardData.style.backgroundImage=`url("${destinationData.image}")`;
+
+        cardData.innerHTML+=
+        `
+        <h2>${destinationData.name}</h2>
+        <p>${destinationData.long_description}</p>
+
+
+        `;
+        let counter=1;
+        destinationData.itinerary.forEach(day=>
+        {
+            cardData.innerHTML+=
+            `
+            <p>Day ${counter} ${day}</p>
+            
+            `;
+            counter++;
+        });
+
+    cardData.innerHTML+=
+    `
+
+    <p>Latitude: ${destinationData.details.location.latitude}</p>
+    <p>Longitude: ${destinationData.details.location.longitude}</p>
+
+
+    `;
+    }
+    catch(error)
+    {
+        console.error("error fetching data", error);
+
+    }
+}
+
+
+
+
+
 
 let destinationData=JSON.parse(sessionStorage.getItem("locationData"));
 let cardData=document.getElementById(destinationData.id);
@@ -72,21 +120,26 @@ cardData.style.backgroundImage=`url("${destinationData.image}")`;
 
 
     `
+    let counter=1;
     destinationData.itinerary.forEach(day=>{
         cardData.innerHTML+=
         `
-        <p>${day}<p>
+        <p>Day ${counter} ${day}</p>
         
-        `
+        `;
+        counter++;
     });
 
     cardData.innerHTML+=
     `
+
     <p>Latitude: ${destinationData.details.location.latitude}</p>
     <p>Longitude: ${destinationData.details.location.longitude}</p>
 
 
-    `
+    `;
+
+
 
 
 
@@ -103,47 +156,6 @@ cardData.style.backgroundImage=`url("${destinationData.image}")`;
 */
 
 
-async function openPage(){
-    let dataResponse= await fetch("./data.json");
-    let destData= await dataResponse.json();
-    let fillData="";
-    document.getElementById('destination').addEventListener("click",function(action){
-        if(action.target.classList.contains('card'))
-            window.location.href=`destination.html`;
-        let cardID= parseInt(action.target.id); //set a variable equal to whatever the person clicked 
-        let correctID=destData.find(destination=>destination.id===cardID); //this line will allow us to gain access to the specific object because of the find function
-        //find function returns the first value that matches the condition
-        const cardData=document.getElementById(cardID); //gain access to html so we can insert data under the correct div
-        cardData.style.backgroundImage = `url("${correctID.image}")`; //insert the image 
-
-        cardData.innerHTML+=`
-            <h2>${correctID.name}<h2>
-            <p>${correctID.details.long_description}</p>
-            
-          `;
-
-            correctID.details.itinerary.forEach(day=>{
-                cardData.innerHTML+=`
-                <p>${day}</p>
-                `
-            });
-            cardData.innerHTML+=`
-            <p>${correctID.details.itinerary.location.latitude}</p>
-            <p>${correctID.details.itinerary.location.longitude}</p>
-
-            
-          `;
-
-
-          
-
-
-
-    
-
-
-    });
-}
 
     
     
